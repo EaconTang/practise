12af6c312f3c28906882393f15de0b04d705065b
eacon <tyingk@gmail.com>
2015-04-15 22:22:37 +0800

    改用面向对象方法重写

--- ShowMeTheCode/0000.py
+++ ShowMeTheCode/0000.py
@@ -7,47 +7,39 @@
 
 from PIL import Image,ImageDraw,ImageFont
 from random import randint
-'''
-def add_avatar(number=str(randint(0,99999)),color=(255,0,0),font='Arial.ttf',size=30):
-	try:
-		print "haha"
-		img = Image.open('test.jpg')
-		toDraw = ImageDraw.Draw(img)
-		theFont = ImageFont.truetype(font,size)
-		toDraw.text((10,10),number,color,theFont)
-		img.save('Modified_avatar_%d.jpg'%number)
 
-		img.show()
-	except BaseException, e:
-		print e
+class Modify_avatar:
+    def __init__(self):
+        pass
+    #打开文件
+    def open(self,filename):
+        self.Img = Image.open(filename)
+    #设置字体位置
+    def setLocate(self,a,b):
+        self.Locate = (a,b)
+    #设置字体内容
+    def setText(self,text):
+        self.Text = str(text)
+    #设置字体颜色
+    def setColor(self,a,b,c):
+        self.Color = (a,b,c)
+    #设置字体格式
+    def setFont(self,font,size):
+        self.Font = ImageFont.truetype(font,size)
+    #编辑并保存图片
+    def draw_save(self):
+        toDraw = ImageDraw.Draw(self.Img)
+        toDraw.text(self.Locate,self.Text,self.Color,self.Font)
+        self.Img.save('%s.jpg'%self.Text)
+        print "Done!"
 
 if __name__ == '__main__':
-	add_avatar()
-'''
-original_avatar = 'test.jpg'
-saved_avatar = 'new_avatar.png'
-windows_font = 'Arial.ttf'
-color = (255, 0, 0)
+	m1 = Modify_avatar()
+    m1.open('download.jpeg')
+    m1.setLocate(10,10)
+    m1.setText(9)
+    m1.setColor(255,0,0)
+    m1.setFont('Arial.ttf',35)
+    m1.draw_save()
 
-def draw_text(text, fill_color, windows_font):
-    try:
-        im = Image.open(original_avatar)
-        x, y =  im.size
-        print "The size of the Image is: "
-        print(im.format, im.size, im.mode)
-        im.show()
-        
-        draw = ImageDraw.Draw(im)
-        font = ImageFont.truetype(windows_font, 35)
-        draw.text((x-20, 7), text, fill_color, font)
-        
-        im.save(saved_avatar)
-        im.show()
 
-    except:
-        print "Unable to load image"
-
-if __name__ == "__main__":
-    number = str(raw_input('please input number: '))
-    #number = str(4)
-    draw_text(number, color, windows_font)
