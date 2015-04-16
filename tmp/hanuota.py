5589fb9372aed004a9beb9c551cdb762b9fccfd1
eacon <tyingk@gmail.com>
2015-04-16 10:31:38 +0800

    message1

--- tmp/hanuota.py
+++ tmp/hanuota.py
@@ -1,3 +1,6 @@
+#coding=utf-8
+#汉诺塔思想：将n个盘子由a移到b，借助中介m；n>1的话，先把a上的n－1个移到m，再把剩下的一个移到b；再把m上的n－1个移回b；递归
+
 def han(n,a,b,m):
     if n == 1:
         print a,'->',b
@@ -7,4 +10,4 @@ def han(n,a,b,m):
         han(n-1,m,b,a)
 
 if __name__ =='__main__':
-    han(10,'A','B','M')
+    han(5,'左','右','中')
